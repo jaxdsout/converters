7f790f07cae2834c6786127866a1dcad3e77c223
jaxdsout <jaxon.southern@mac.com>
2024-06-12 20:53:31 -0500

    final price added

--- xlsx/xlsx_convert.py
+++ xlsx/xlsx_convert.py
@@ -24,7 +24,6 @@ for filepath in filepaths:
     headers = [column.replace("_", " ").title() for column in columns]
 
     pdf.set_font(family='Arial', size=8, style='B')
-    pdf.set_text_color(100, 100, 100)
     pdf.cell(w=30, h=8, txt=headers[0], border=1)
     pdf.cell(w=70, h=8, txt=headers[1], border=1)
     pdf.cell(w=30, h=8, txt=headers[2], border=1)
@@ -33,12 +32,25 @@ for filepath in filepaths:
 
     for index, row in df.iterrows():
         pdf.set_font(family='Arial', size=8)
-        pdf.set_text_color(100, 100, 100)
         pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
         pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    total = df['total_price'].sum()
+    pdf.set_font(family='Arial', size=8)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total), border=1, ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=10, txt=f"Total Due: {total}", ln=1)
+    pdf.cell(txt='your company', w=50, ln=1)
+    pdf.ln(5)
+    pdf.image("logo.png", w=10)
 
     pdf.output(f"pdfs/{filename}.pdf")
 
